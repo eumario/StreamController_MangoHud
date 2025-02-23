c334de93201d6908934f94aa225926c650ccdd10
Mario Steele <mario@ruby-im.net>
2025-02-23 01:10:02 -0600

    Updated Main
    Due to how Flatpak is a security jail for processes, added detection if we are in a flatpak, if we are in flatpak, we use new custom launch_backend function called flatpak_launch_backend.
    
    flatpak_launch_backend(): Does the same thing as launch_backend(), except instead of normal invocation of the backend.py script, we launch the backend.py script through flatpak-spawn --host, to have the backend run in the system context, which should solve issues with access to /tmp folder, as well as process information for watching log files.

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from src.backend.PluginManager.EventHolder import EventHolder
 from src.backend.PluginManager.ActionHolder import ActionHolder
 
 import os
+import subprocess
+from loguru import logger as log
 
 # Import Actions
 from .actions.ItemDisplay import ItemDisplay
 from .actions.LaunchMango import LaunchMango
 
-class PluginWebsocket(PluginBase):
+class PluginMangoHud(PluginBase):
     def __init__(self):
         super().__init__()
 
         # Launch backend
         backend_path = os.path.join(self.PATH, "backend", "backend.py")
-        self.launch_backend(backend_path=backend_path, open_in_terminal=False, venv_path=os.path.join(self.PATH, "backend", ".venv"))
+        if is_in_flatpak():
+            self.flatpak_launch_backend(backend_path=backend_path, open_in_terminal=False, venv_path=os.path.join(self.PATH, "backend", ".venv"))
+        else:
+            self.launch_backend(backend_path=backend_path, open_in_terminal=False, venv_path=os.path.join(self.PATH, "backend", ".venv"))
         self.wait_for_backend(5)
 
         # Register Actions
@@ -50,3 +55,27 @@ class PluginWebsocket(PluginBase):
             plugin_version = "0.1.0",
             app_version = "1.5.0-beta6"
         )
+
+    def flatpak_launch_backend(self, backend_path: str, venv_path: str = None, open_in_terminal: bool = False) -> None:
+        self.start_server()
+        port = self.server.port
+
+        # Construct the command to launch the backend
+        if open_in_terminal:
+            command = "gnome-terminal -- bash -c '"
+            if venv_path is not None:
+                command += f". {venv_path}/bin/activate && "
+            command += f"python3 {backend_path} --port={port}; exec $SHELL'"
+        else:
+            command = ""
+            if venv_path is not None:
+                command = f". {venv_path}/bin/activate && "
+            command += f"python3 {backend_path} --port={port}"
+
+        log.info(f"Launching backend: {command}")
+        subprocess.Popen(f"flatpak-spawn --host {command}", shell=True, start_new_session=open_in_terminal)
+
+        self.wait_for_backend()
+
+def is_in_flatpak() -> bool:
+    return os.path.isfile('/.flatpak-info')
